54698d0b83a37bda52e1c85bb4954551263d5a0b
Brandon R. India <brandon.r.india@gmail.com>
2021-05-08 17:50:13 -0500

    updated to 20.04. changed build+run commands.

--- containers/rubyworld/README.md
+++ containers/rubyworld/README.md
@@ -1,2 +1,2 @@
-Build with: $(. ./buildcommand.sh)
-Run with $(. ./runcommand.sh)
+Build & run with:
+  TAG=20.04 && RUBY_VER=ruby-2.7.3 && RAILS_VER=latest . ./buildcommand.sh && . ./runcommand.sh
--- containers/rubyworld/runcommand.sh
+++ containers/rubyworld/runcommand.sh
@@ -1,1 +1,1 @@
-run -u ${CUSER} --env=${ENV} --app=${APP} -I -p=3000:3000 --name=${APP} local/rubyworld
+if [ ${TAG} ]; then TAG=":${TAG}"; fi && run -u ${CUSER} --env=${ENV} --app=${APP} -I -p=3000:3000 --name=${APP} --rm local/rubyworld${TAG}
